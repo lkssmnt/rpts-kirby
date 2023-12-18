cef0c83888469a7274e7ff63d01aa6f340048a62
lkssmnt <hello@lsiemoneit.de>
2023-12-18 16:24:19 +0100

    person

---
+++ site/templates/person.php
@@ -0,0 +1,41 @@
+<?php snippet("head") ?>
+
+<h1><?= $page->title() ?></h1>
+
+<?php if ($page->email()->isNotEmpty()) : ?>
+  <p>Email-Adresse: <?= $page->email() ?></p>
+<?php endif ?>
+
+
+<!-- Projekte: -->
+
+<ul>
+  <?php
+  $allProjects = $site->find("projects")->children();
+
+  $filteredProjects = $allProjects->filter(
+    fn ($projectPage) => $projectPage->author()->toPages()->has($page)
+  );
+
+  foreach ($filteredProjects as $project) : ?>
+    <li>
+      <a href="<?= $project->url() ?>">
+        <?= $project->title() ?>
+      </a>
+    </li>
+  <?php endforeach ?>
+</ul>
+
+
+<?php snippet("foot") ?>
+
+
+<?php
+
+// $projects = $allProjects->filter(function ($child) use ($page) {
+//   return $child->author()->toPage()->is($page);
+// });
+
+// dump($projects);
+
+?>
