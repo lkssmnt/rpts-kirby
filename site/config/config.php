52a4d0979eb995a82fb558d567d86dfd61abd5ce
lkssmnt <hello@lsiemoneit.de>
2023-12-18 16:24:09 +0100

    srcset

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
-  'debug' => true
+  'debug' => true,
+  'thumbs' => [
+    'srcsets' => [
+      'projectImages' => [
+        '800w' => ['width' => 800, 'quality' => 80],
+        '1024w' => ['width' => 500, 'quality' => 80],
+        '1440w' => ['width' => 700, 'quality' => 80],
+        '2048w' => ['width' => 1000, 'quality' => 80]
+      ],
+    ]
+  ],
 ];
--- site/templates/project.php
+++ site/templates/project.php
@@ -2,6 +2,12 @@
 
 <h1><?= $page->title() ?></h1>
 
+<div class="images">
+  <?php foreach ($page->gallery()->toFiles() as $image) : ?>
+    <img src="<?= $image->url() ?>" srcset="<?= $image->srcset("projectImages") ?>">
+  <?php endforeach ?>
+</div>
+
 <p>Autor:innen:</p>
 <ul>
   <?php foreach ($page->author()->toPages() as $author) : ?>
