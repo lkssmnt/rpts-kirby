533acbe56987a9ebac73478a390c4d54dc44f704
lkssmnt <hello@lsiemoneit.de>
2023-12-18 16:23:48 +0100

    added javascript interaction

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,15 +6,40 @@ $projects = $site->find("projects")->children();
 
 <h1><?= $site->title() ?></h1>
 
+<p class="author-info"></p>
+
+<img class="hover-image" src="https://placehold.co/600x400">
+
 <h2>Projekte:</h2>
 <ul>
   <?php foreach ($projects as $project) : ?>
-    <li>
+    <li data-imageurl="<?= $project->gallery()->first()->toFile()->url() ?>" data-author="<?= $project->author()->first()->toPage()->title() ?>">
       <a href="<?= $project->url() ?>">
         <?= $project->title() ?>
       </a>
     </li>
   <?php endforeach ?>
 </ul>
+
+<script>
+  const authorInfo = document.querySelector(".author-info");
+  const hoverImage = document.querySelector(".hover-image");
+  window.addEventListener("mousemove", (e) => {
+    hoverImage.style.left = e.pageX + "px";
+    hoverImage.style.top = e.pageY + "px";
+  });
+
+  const projectLinks = document.querySelectorAll("li");
+  projectLinks.forEach((link) => {
+    link.addEventListener("mouseenter", (event) => {
+      const imageUrl = event.target.dataset.imageurl;
+      hoverImage.src = imageUrl;
+
+      const author = event.target.dataset.author;
+      authorInfo.innerHTML = author;
+    });
+  });
+
+</script>
 
 <?php snippet("foot") ?>
