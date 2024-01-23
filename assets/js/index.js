0ef15f912a009989667c6d9f2b8d224f0e217d0b
lkssmnt <hello@lsiemoneit.de>
2024-01-23 22:56:54 +0100

    added multi drag

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,8 +16,67 @@ window.addEventListener("DOMContentLoaded", () => {
     preparePrintInterface();
     createCollection(".collection-wrapper");
   }
+
+  const allDragElements = document.querySelectorAll(".draggable");
+  allDragElements.forEach((element) => dragElement(element));
+  
 });
 
+
+
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  elmnt.onmousedown = dragMouseDown;
+
+  elmnt.addEventListener("mousedown", (event) => {
+    event.stopPropagation();
+
+    console.log(elmnt);
+
+    document.querySelectorAll(".draggable").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    elmnt.classList.add("active");
+  });
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    console.log("end of drag");
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -4,21 +4,9 @@
 $projects = $site->find("projekte")->children();
 ?>
 
-<h1><?= $site->title() ?></h1>
-
-<p class="author-info"></p>
-
-<img class="hover-image" src="https://placehold.co/600x400">
-
-<h2>Projekte:</h2>
-<ul>
-  <?php foreach ($projects as $project) : ?>
-    <li data-imageurl="<?= $project->gallery()->first()->toFile()->url() ?>" data-author="<?= $project->author()->first()->toPage()->title() ?>">
-      <a href="<?= $project->url() ?>">
-        <?= $project->title() ?>
-      </a>
-    </li>
-  <?php endforeach ?>
-</ul>
+<main class="draggable">
+  <div class="projekt draggable">Test</div>
+  <div class="projekt draggable">Test 2</div>
+</main>
 
 <?php snippet("foot") ?>
